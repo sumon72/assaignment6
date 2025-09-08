50b1c80a6d8007b4beb67b571c101e1d046788e9
sumon72 <sumonfb72@gmail.com>
2025-09-08 20:40:38 +0600

    update catagory and all plant data show by api

--- js/greenearth.js
+++ js/greenearth.js
@@ -1,6 +1,79 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-
+    getCatagoryData();
+    getAllItemData();
 
 });
+
+const getCatagoryData = async () => {
+    try {
+        const response = await fetch("https://openapi.programming-hero.com/api/categories", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+        const data = await response.json();
+        const CatagolyList = data.categories;
+
+        const catul = document.getElementById("catagory");
+        let HtmlContent = "";
+
+        for (let cat of CatagolyList) {
+
+            HtmlContent += ` <li class="cursor-pointer px-3 py-2 rounded-md hover:bg-[#166534] hover:text-[#FFFFFF]" id="${cat.id}">${cat.category_name}</li>`;
+
+        }
+
+        catul.innerHTML = HtmlContent;
+
+    } catch (error) {
+        console.error("Fetch error:", error);
+    }
+};
+
+const getAllItemData = async () => {
+    try {
+        const response = await fetch("https://openapi.programming-hero.com/api/plants", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+        const data = await response.json();
+        const AllItemList = data.plants;
+
+        const CardItem = document.getElementById("itemCard");
+        let HtmlContent = "";
+
+        for (let itm of AllItemList) {
+
+            HtmlContent += `<div class="bg-white shadow-md rounded-xl p-4 flex flex-col">
+                        <img src=${itm.image} alt=${itm.name} class="w-full h-40 object-cover rounded-lg mb-3">
+                        <h3 class="text-lg font-semibold text-gray-800" command="show-modal" commandfor="dialog">${itm.name}</h3>
+                        <p class="text-sm text-gray-600 mb-3">${itm.description}</p>
+                        <div class="flex items-center justify-between mb-3">
+                            <span class="bg-green-100 text-green-700 text-xs px-3 py-1 rounded-full">${itm.category}</span>
+                            <span class="text-lg font-bold text-gray-900">৳${itm.price}</span>
+                        </div>
+                        <button command="show-modal" commandfor="dialog"
+                            class="mt-auto bg-[#166534] text-white py-2 px-4 rounded-lg hover:bg-green-700">
+                            Add to Cart
+                        </button>
+                    </div>`;
+
+        }
+
+        CardItem.innerHTML = HtmlContent;
+
+    } catch (error) {
+        console.error("Fetch error:", error);
+    }
+};
 
