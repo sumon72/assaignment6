cbb603347740895b49041fe52a28bf210dfbdc3c
sumon72 <sumonfb72@gmail.com>
2025-09-08 19:37:04 +0600

    update modal

--- js/greenearth.js
+++ js/greenearth.js
@@ -1,6 +1,18 @@
 
+function openModal() {
+    document.getElementById('treeModal').classList.remove('hidden');
+    document.getElementById('treeModal').classList.add('flex');
+}
+
+function closeModal() {
+    document.getElementById('treeModal').classList.remove('flex');
+    document.getElementById('treeModal').classList.add('hidden');
+}
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-   
-    
+
+
 });
 
